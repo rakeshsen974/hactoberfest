d3af30b41f4b5320df4c0ead8bcd1796d2b4ce89
rakeshsen974 <116906502+rakeshsen974@users.noreply.github.com>
2023-10-04 21:30:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let register_btn = document.getElementById("register_btn");
+let forgate_Btn = document.getElementById("forgot_btn");
+let loginform = document.getElementById("loginform");
+
+
+register_btn.onclick = function(){
+    loginform.style.display="none";
+}
