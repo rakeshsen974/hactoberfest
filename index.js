485290af6b376fbf211b586b9a081830d2b150c3
Trivendra <113928900+trivendrasen53@users.noreply.github.com>
2023-10-04 21:58:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-let register_btn = document.getElementById("register_btn");
-let forgate_Btn = document.getElementById("forgot_btn");
-let loginform = document.getElementById("loginform");
+let register_Button = document.getElementById("register_Button");
+let forgot_Button = document.getElementById("forgot_Button");
+let login_Button = document.getElementById("login_Button");
 
 
 register_btn.onclick = function(){
     loginform.style.display="none";
 }
+register_btn.onclick = function(){
+    loginform.style.display="
+        None";
+}
+if{
+    register_btn.onclick = function(){
+    loginform.style.display="
+        None";
+    }
